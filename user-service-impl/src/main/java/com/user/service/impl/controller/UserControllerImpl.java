af88fdfb2318f9594e369801745e9ebc23d9652c
daviddai <david.dai.personal@gmail.com>
2018-07-23 23:10:28 +1000

    enable cross origin

--- user-service-impl/src/main/java/com/user/service/impl/controller/UserControllerImpl.java
+++ user-service-impl/src/main/java/com/user/service/impl/controller/UserControllerImpl.java
@@ -4,9 +4,11 @@ import com.user.serivce.api.controller.UserController;
 import com.user.serivce.api.controller.model.UserLogin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class UserControllerImpl implements UserController {
 
     @Override
