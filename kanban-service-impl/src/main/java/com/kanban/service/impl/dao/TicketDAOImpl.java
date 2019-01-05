6028be4f8dcde02dad24f46905c0d0326ab3c7f7
daviddai <david.dai.personal@gmail.com>
2019-01-05 12:55:28 +1100

    fixed bug for adding new ticket

--- kanban-service-impl/src/main/java/com/kanban/service/impl/dao/TicketDAOImpl.java
+++ kanban-service-impl/src/main/java/com/kanban/service/impl/dao/TicketDAOImpl.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 @Repository
@@ -38,9 +40,18 @@ public class TicketDAOImpl implements TicketDAO {
                                                               "LEFT JOIN task ta " +
                                                               "ON ti.id = ta.ticket_id";
 
-    final private static String FIND_TICKET_BY_ID = "SELECT id, title, description, status " +
-                                                    "FROM ticket " +
-                                                    "WHERE id = :id";
+    final private static String FIND_TICKET_BY_ID = "SELECT " +
+                                                    "       ti.id as ticket_id, " +
+                                                    "       ti.title as ticket_title, " +
+                                                    "       ti.description as ticket_description, " +
+                                                    "       ti.status as ticket_status, " +
+                                                    "       ta.id as task_id, " +
+                                                    "       ta.name as task_name, " +
+                                                    "       ta.done as task_done " +
+                                                    "FROM ticket ti " +
+                                                    "LEFT JOIN task ta " +
+                                                    "ON ti.id = ta.ticket_id " +
+                                                    "WHERE ti.id = :ticket_id";
 
     final private static String UPDATE_TICKET_BY_ID = "UPDATE ticket " +
                                                       "SET title = :title, description = :description, status = :status " +
@@ -52,15 +63,16 @@ public class TicketDAOImpl implements TicketDAO {
         namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
         simpleJdbcInsert = new SimpleJdbcInsert(dataSource);
         simpleJdbcInsert.setTableName("ticket");
+        simpleJdbcInsert.setGeneratedKeyName("id");
     }
 
     @Override
-    public void add(Ticket ticket) {
+    public long add(Ticket ticket) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("title", ticket.getTitle());
         parameters.put("description", ticket.getDescription());
         parameters.put("status", ticket.getStatus().getDescription());
-        simpleJdbcInsert.execute(parameters);
+        return (long) simpleJdbcInsert.executeAndReturnKey(parameters);
     }
 
     @Override
@@ -71,29 +83,9 @@ public class TicketDAOImpl implements TicketDAO {
             while (resultSet.next()) {
                 long ticketId = resultSet.getLong("ticket_id");
 
-                Ticket ticket = new Ticket();
-
-                if (tickets.containsKey(ticketId)) {
-                    ticket = tickets.get(ticketId);
-                } else {
-                    String ticketTitle = resultSet.getString("ticket_title");
-                    String ticketDescription = resultSet.getString("ticket_description");
-                    String ticketStatus = resultSet.getString("ticket_status");
-
-                    ticket.setId(ticketId);
-                    ticket.setTitle(ticketTitle);
-                    ticket.setDescription(ticketDescription);
-                    ticket.setStatus(TicketStatus.getTicketStatus(ticketStatus));
-
-                    tickets.put(ticketId, ticket);
-                }
-
-                long taskId = resultSet.getLong("task_id");
-                if (!resultSet.wasNull()) {
-                    String taskName = resultSet.getString("task_name");
-                    boolean done = resultSet.getBoolean("task_done");
-                    ticket.getTasks().add(new Task(taskId, taskName, done, ticketId));
-                }
+                Ticket ticket = tickets.get(ticketId);
+                ticket = setTicket(resultSet, ticket);
+                tickets.put(ticket.getId(), ticket);
             }
 
             return new ArrayList<>(tickets.values());
@@ -102,21 +94,19 @@ public class TicketDAOImpl implements TicketDAO {
 
     @Override
     public Optional<Ticket> findById(long id) {
-        SqlParameterSource sqlParameterSource = new MapSqlParameterSource().addValue("id", id);
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource().addValue("ticket_id", id);
 
-        Ticket ticket = namedParameterJdbcTemplate.query(FIND_TICKET_BY_ID, sqlParameterSource, resultSet -> {
-            if (resultSet.next()) {
-                long ticketId = resultSet.getLong("id");
-                String ticketTitle = resultSet.getString("title");
-                String ticketDescription = resultSet.getString("description");
-                String ticketStatus = resultSet.getString("status");
-                return new Ticket(ticketId, ticketTitle, ticketDescription, TicketStatus.getTicketStatus(ticketStatus), new ArrayList<>());
-            } else {
-                return null;
+        Ticket ticketWithTasks = namedParameterJdbcTemplate.query(FIND_TICKET_BY_ID, sqlParameterSource, resultSet -> {
+            Ticket ticket = null;
+
+            while (resultSet.next()) {
+                ticket = setTicket(resultSet, ticket);
             }
+
+            return ticket;
         });
 
-        return ticket != null ? Optional.of(ticket) : Optional.empty();
+        return ticketWithTasks != null ? Optional.of(ticketWithTasks) : Optional.empty();
     }
 
     @Override
@@ -126,6 +116,31 @@ public class TicketDAOImpl implements TicketDAO {
                                                                            .addValue("status", ticket.getStatus().getDescription())
                                                                            .addValue("id", ticket.getId());
         namedParameterJdbcTemplate.update(UPDATE_TICKET_BY_ID, sqlParameterSource);
+    }
+
+    private Ticket setTicket(ResultSet resultSet, Ticket ticket) throws SQLException {
+        if (ticket == null) {
+            ticket = new Ticket();
+
+            long ticketId = resultSet.getLong("ticket_id");
+            String ticketTitle = resultSet.getString("ticket_title");
+            String ticketDescription = resultSet.getString("ticket_description");
+            String ticketStatus = resultSet.getString("ticket_status");
+
+            ticket.setId(ticketId);
+            ticket.setTitle(ticketTitle);
+            ticket.setDescription(ticketDescription);
+            ticket.setStatus(TicketStatus.getTicketStatus(ticketStatus));
+        }
+
+        long taskId = resultSet.getLong("task_id");
+        if (!resultSet.wasNull()) {
+            String taskName = resultSet.getString("task_name");
+            boolean done = resultSet.getBoolean("task_done");
+            ticket.getTasks().add(new Task(taskId, taskName, done, ticket.getId()));
+        }
+
+        return ticket;
     }
 
 }
